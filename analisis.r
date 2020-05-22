78b2a42877ed245d5b8b37959a88d0f40f179410
Francisco Salas <fsalasi93@gmail.com>
2020-05-22 23:34:17 +0200

    Transofrmación de los datos a formato "tidy" y reformato de la gráfica

--- analisis.r
+++ analisis.r
@@ -4,13 +4,17 @@ library(tidyverse)
 # import aemet data
 data = read.csv("aemet_data.csv", row.names = 1)
 data = data %>% mutate(fechahora=as.POSIXct(fechahora, format="%Y-%m-%d %H:%M:%S"))
+data_spread = data
+data_gathered = data %>% gather(key = "temp_type", value = "value", -fechahora)
 
 # plot data
-data %>% ggplot(aes(x = fechahora, y = tinst)) + 
-  geom_line(aes(y = tmax, color = "tmax"), size = 2) + 
-  geom_line(aes(y = tmin, color = "tmin"), size = 2) + 
-  geom_ribbon(aes(ymin=tmin,ymax=tmax), fill="grey") + 
-  geom_point() + 
-  theme_light() + theme(legend.position = c(0.90,0.11), legend.title = element_blank(), legend.background = element_blank()) +  
+ggplot() + 
+  geom_ribbon(data = data_spread, aes(x = fechahora, ymin=tmin, ymax=tmax), fill = "grey") + 
+  geom_line(data = (data_gathered %>% filter(temp_type != "tinst")), aes(x = fechahora, y = value, color = temp_type), size = 1) +
+  geom_point(data = (data_gathered %>% filter(temp_type == "tinst")), aes(x = fechahora, y = value, color = temp_type), show.legend = FALSE) +
+  theme_light() + 
+  theme(legend.position = c(0.11,0.85), legend.title = element_blank(), legend.background = element_blank()) +  
+  scale_color_manual(values=c("#FFF684", "#DA3612", "#00D8FF")) +
+  scale_x_datetime(date_breaks = "1 days", date_labels = "%b %d") +
   xlab("") + ylab ("Temperatura (ºC)") + ggtitle("Temperaturas en est. AEMET Andújar")
   
